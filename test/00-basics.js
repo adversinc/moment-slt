e4da7966b14f088bcd1ddd4d7c1bc2f892306faa
Dino Fabrello <oleg@borodkin.ru>
2021-04-20 15:46:01 +0300

    Fix tests

--- test/00-basics.js
+++ test/00-basics.js
@@ -3,83 +3,28 @@ const
 	assert = require('assert');
 
 // Libs to test
-const SLT = require("../lib/index");
+const moment = require("../lib/index");
 
 // Tests
-describe('normalize_slname', function() {
-	it('should normalize old name', function() {
-		const n = SLTools.normalize_slname("Fname Lname");
+describe('time difference', function() {
+	it('should return correct difference with UTC', function() {
+		const now = new Date();
+		const n = moment.tz(new Date(), "America/Los_Angeles");
 
-		assert.equal(n, "Fname Lname");
-	});
-	it('should normalize new name', function() {
-		const n = SLTools.normalize_slname("NewName");
+		let diff = n.hours() - now.getUTCHours();
+		let shouldBeDate = now.getUTCDate();
 
-		assert.equal(n, "NewName Resident");
-	});
-});
+		if(diff > 12) {
+			shouldBeDate--;
+			diff -= 24;
+		}
 
-describe('slname2str', function() {
-	it('should stringify old name', function() {
-		const n = SLTools.slname2str("Fname Lname");
+		console.log("SLT: " + n.toString());
 
-		assert.equal(n, "Fname Lname");
-	});
-	it('should stringify new short name', function() {
-		const n = SLTools.slname2str("NewName");
-
-		assert.equal(n, "NewName");
-	});
-	it('should stringify new complete name', function() {
-		const n = SLTools.slname2str("NewName Resident");
-
-		assert.equal(n, "NewName");
+		// Check if components of the date are correct
+		assert.equal(diff, -7);
+		assert.equal(n.minute(), now.getUTCMinutes());
+		assert.equal(n.second(), now.getUTCSeconds());
+		assert.equal(n.date(), shouldBeDate);
 	});
 });
-
-
-describe('split_slname', function() {
-	it('should split old name', function() {
-		const n = SLTools.split_slname("Fname Lname");
-
-		assert.deepEqual(n, ["Fname", "Lname"]);
-	});
-	it('should split new short name', function() {
-		const n = SLTools.split_slname("NewName");
-
-		assert.deepEqual(n, ["NewName", "Resident"]);
-	});
-	it('should split new complete name', function() {
-		const n = SLTools.split_slname("NewName Resident");
-
-		assert.deepEqual(n, ["NewName", "Resident"]);
-	});
-});
-
-
-describe('checkSLName', function() {
-	it('should succeed', function() {
-		assert.ok(SLTools.checkSLName("Fname Lname"));
-		assert.ok(SLTools.checkSLName("Fname"));
-		assert.ok(SLTools.checkSLName("Fname22 Lname"));
-		assert.ok(SLTools.checkSLName("Fname_22 Lname"));
-	});
-	it('should fail', function() {
-		assert.ok(!SLTools.checkSLName("Fname@22 Lname"));
-		assert.ok(!SLTools.checkSLName("2Fname Lname"));
-		assert.ok(!SLTools.checkSLName("2Fname"));
-	});
-});
-
-
-describe('equalSLName', function() {
-	it('should succeed', function() {
-		assert.ok(SLTools.equalSLName("Fname Lname", "fname lname"));
-		assert.ok(SLTools.equalSLName("Fname", "fname resident"));
-		assert.ok(SLTools.equalSLName("Fname", "fname Resident"));
-	});
-	it('should fail', function() {
-		assert.ok(!SLTools.equalSLName("Fname", "fname2 Resident"));
-	});
-});
-
